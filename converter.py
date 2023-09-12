edded7d843eb123106bd6daa3d0ee0cc12298ef8
FAZuH <fazuhhh@gmail.com>
2023-09-13 01:08:13 +0700

    - Image input files now needs to be placed in input_image folder - Converted images are now moved from input_image folder into output_image folder - Output ico files are now created in output_ico folder

--- converter.py
+++ converter.py
@@ -1,32 +1,41 @@
 import os
 import colorama
+import pathlib
+import shutil
 from PIL import Image
+
+BASE_DIR = pathlib.Path(__file__).parent
+INPUT_IMAGE_DIR = BASE_DIR / 'input_image'
+OUTPUT_ICO_DIR = BASE_DIR / 'output_ico'
+OUTPUT_IMAGE_DIR = BASE_DIR / 'output_image'
 
 
 class Main:
     def __init__(self):
         colorama.init()
-        self.image_files: list[None | str] = []
+        self.image_paths: list[None | str] = []
         self.run()
 
     def run(self):
         self.get_image_paths()
         self.convert_to_ico()
+        self.move_old_image()
         input()
 
     def get_image_paths(self):
         image_count: int = 0
-        files = os.listdir()
+        files = os.listdir(INPUT_IMAGE_DIR)
         for file_name in files:
-            if os.path.isfile(file_name) is False:
+            file_path = INPUT_IMAGE_DIR / file_name
+            if os.path.isfile(file_path) is False:
                 continue
 
-            _, file_extension = os.path.splitext(file_name)
+            _, file_extension = os.path.splitext(file_path)
             if file_extension not in {'.jpg', '.png'}:
                 continue
 
-            self.image_files.append(file_name)
-            print(colorama.Fore.YELLOW + f'Found valid file "{file_name}".' + colorama.Style.RESET_ALL)
+            self.image_paths.append(file_path)
+            print(colorama.Fore.YELLOW + f'Found valid file "{file_path}".' + colorama.Style.RESET_ALL)
             image_count += 1
 
         print(colorama.Fore.YELLOW + f'Found {image_count} .jpg or .png files.' + colorama.Style.RESET_ALL)
@@ -35,17 +44,17 @@ class Main:
         print(colorama.Fore.YELLOW + f'Converting...' + colorama.Style.RESET_ALL)
         convert_count: int = 0
         error_count: int = 0
-        for image_file in self.image_files:
+        for image_path in self.image_paths:
             try:
-                image = Image.open(image_file)
+                image = Image.open(image_path)
                 if image.mode != 'RGBA':
                     image = image.convert('RGBA')
 
-                image_name, _ = os.path.splitext(image_file)
-                ico_name = f"{image_name}.ico"
-                image.save(ico_name, format="ICO", sizes=[(32, 32)])
+                image_name = os.path.splitext(os.path.basename(image_path))[0]
+                ico_path = OUTPUT_ICO_DIR / f"{image_name}.ico"
+                image.save(ico_path, format='ICO', sizes=[(32, 32)])
                 convert_count += 1
-                print(colorama.Fore.GREEN + f'Converted {image_file} into .ico' + colorama.Style.RESET_ALL)
+                print(colorama.Fore.GREEN + f'Converted {image_path} into .ico' + colorama.Style.RESET_ALL)
             except Exception as e:
                 error_count += 1
                 print(colorama.Fore.RED + f'An error occurred: {str(e)}' + colorama.Style.RESET_ALL)
@@ -56,3 +65,11 @@ class Main:
             f'{f" with {error_count} errors" if error_count > 0 else ""}.'
             + colorama.Style.RESET_ALL
         )
+
+    def move_old_image(self):
+        try:
+            for image_file in self.image_paths:
+                image_path = INPUT_IMAGE_DIR / image_file
+                shutil.move(image_path, OUTPUT_IMAGE_DIR)
+        except Exception as e:
+            print(colorama.Fore.RED + f'An error occured: {e}' + colorama.Style.RESET_ALL)
